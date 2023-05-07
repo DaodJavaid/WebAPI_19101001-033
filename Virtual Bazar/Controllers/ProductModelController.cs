c700becab59f3a0178a293fe4c39e4a8741483cd
Muhammad Daod Javaid <muhammaddaodjavaid@gmail.com>
2023-05-07 14:18:06 +0500

    Add project files.

---
+++ Virtual Bazar/Controllers/ProductModelController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Virtual_Bazar.Data;
+using Virtual_Bazar.Models;
+
+namespace Virtual_Bazar.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductModelController : Controller
+    {
+
+        private readonly ProductDbContext _productDbContext;
+
+        public ProductModelController(ProductDbContext productDbContext)
+        {
+            _productDbContext = productDbContext;
+        }
+
+        [HttpPut("Add/{id}/{name}/{Description}")]
+        public IActionResult PutData(ProductModel product, int id, string name, string Description)
+        {
+            // Validate the model if necessary
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            product.Id = id;
+            product.Name = name;
+            product.Description = Description;
+
+            // Save the data to the database
+            _productDbContext.Product_Detail.Add(product);
+            _productDbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            // Retrieve data from the database
+            var data = _productDbContext.Product_Detail.ToList();
+
+            return Ok(data);
+        }
+
+
+    }
+}
---
+++ Virtual Bazar/Data/ProductDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Virtual_Bazar.Models;
+
+namespace Virtual_Bazar.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ProductModel> Product_Detail { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductModel>()
+                .Property(p => p.Id)
+                .UseIdentityColumn()
+                .Metadata.SetBeforeSaveBehavior(PropertySaveBehavior.Ignore);
+
+            modelBuilder.Entity<ProductModel>()
+                .Property(p => p.Id)
+                .ValueGeneratedNever();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public void EnableIdentityInsertForProductDetail()
+        {
+            Database.ExecuteSqlRaw("SET IDENTITY_INSERT dbo.Product_Detail ON");
+        }
+
+        public void DisableIdentityInsertForProductDetail()
+        {
+            Database.ExecuteSqlRaw("SET IDENTITY_INSERT dbo.Product_Detail OFF");
+        }
+    }
+}
